891d0b8ccbf5ed95c6adcc773054627a181afec4
etoribadanai <etoribadanai@gmail.com>
2020-08-21 15:50:54 -0300

    move process

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -2,12 +2,57 @@ defmodule Hangman.Game do
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    letters: []
+    letters: [],
+    used: MapSet.new()
   )
 
-  def new_game() do
+  def new_game(word) do
     %Hangman.Game{
-      letters: Dictionary.random() |> String.codepoints()
+      letters: word |> String.codepoints()
     }
+  end
+
+  def new_game(), do: new_game(Dictionary.random())
+
+  def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
+    {game, tally(game)}
+  end
+
+  def make_move(game, guess) do
+    game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    {game, tally(game)}
+  end
+
+  def accept_move(game, _guess, _already_guesses = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  def accept_move(game, guess, _already_guesses) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
+    |> score_guess(Enum.member?(game.letters, guess))
+  end
+
+  def score_guess(game, _good_guess = true) do
+    new_state =
+      MapSet.new(game.letters)
+      |> MapSet.subset?(game.used)
+      |> maybe_won
+
+    Map.put(game, :game_state, new_state)
+  end
+
+  def score_guess(game = %{turns_left: 1}, _not_good_guess) do
+    Map.put(game, :game_state, :lost)
+  end
+
+  def score_guess(game = %{turns_left: turns_left}, _not_good_guess) do
+    %{game | game_state: :bad_guess, turns_left: turns_left - 1}
+  end
+
+  def maybe_won(true), do: :won
+  def maybe_won(_), do: :good_guess
+
+  def tally(game) do
+    123
   end
 end
